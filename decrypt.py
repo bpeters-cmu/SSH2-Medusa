e1e1309706c85e476523befba16d94b62aa6de57
Ben <Ben@test.com>
2018-02-01 11:28:07 -0600

    fixing path

--- decrypt.py
+++ decrypt.py
@@ -11,8 +11,7 @@ decrypted = decrypt(key, base64.decodebytes(data))
 
 output_file = decrypted.decode("utf-8")
 print(output_file)
-path = '/SSH2-Medusa/medusa_keys/'
-filename = name + '_decrypted'
-f = open(filename, 'w' )
+path = '/SSH2-Medusa/medusa_keys/'+ name + '_decrypted'
+f = open(path, 'w' )
 f.write(output_file)
 f.close()
